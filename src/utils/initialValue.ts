46702e3812c543e522335a04ad0613b154fc3297
Cauacs <cauasene00@hotmail.com>
2023-02-08 23:05:26 +0000

    Changed initial value

--- src/utils/initialValue.ts
+++ src/utils/initialValue.ts
@@ -1,29 +1,76 @@
 import { Descendant } from "slate";
+import { text } from "stream/consumers";
 import { BlockType, CustomElement } from "./slate-types";
 
 export const initialValue: Descendant[] = [
   {
+    type: BlockType.Paragraph,
+    children: [
+      {
+        text: "This is an minimalist open source text editor built with SlateJS. It has rich-text features like: ",
+      },
+      {
+        text: "bold",
+        bold: true,
+      },
+      { text: ", " },
+      {
+        text: "italic",
+        italic: true,
+      },
+      { text: ", " },
+      { text: "underline " },
+      { text: "and " },
+      { text: "strike-through" },
+      { text: "." },
+    ],
+  },
+  {
+    type: BlockType.Paragraph,
+    children: [
+      {
+        text: "It also has markdown like shortcuts like:",
+      },
+    ],
+  },
+  {
+    type: BlockType.H3,
+    children: [{ text: '"#" For headers, ranging from H1 to H4' }],
+  },
+  {
+    type: BlockType.BlockQuote,
+    children: [
+      {
+        text: '">" For block quotes.',
+      },
+    ],
+  },
+  {
+    type: BlockType.ListItem,
+    children: [{ text: '"*"' }],
+  },
+  {
+    type: BlockType.ListItem,
+    children: [{ text: '"+"' }],
+  },
+  {
+    type: BlockType.ListItem,
+    children: [{ text: '"-"' }],
+  },
+  {
+    type: BlockType.ListItem,
+    children: [{ text: '"For list items."' }],
+  },
+  {
+    type: BlockType.Paragraph,
+    children: [
+      {
+        text: "The documents you save get saved to a link so you can revisit or share it with someone",
+      },
+    ],
+  },
+  {
     type: BlockType.H1,
-    children: [
-      {
-        text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-      },
-    ],
-  },
-  {
-    type: BlockType.Paragraph,
-    children: [
-      {
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is not a big text     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-    ],
-  },
-  {
-    type: BlockType.Paragraph,
-    children: [
-      {
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-    ],
+    children: [{ text: "Try it out!" }],
   },
 ];
